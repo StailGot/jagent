1b6cd73cf172aba154ed81a85a3d916114996bc3
stailgot <sanchesz@list.ru>
2016-12-02 02:53:23 +0300

    Daemon respawn

--- jagent/jagent.py
+++ jagent/jagent.py
@@ -3,6 +3,7 @@ import os
 import io
 import subprocess
 import threading
+import multiprocessing
 
 def get_path():
   if len( sys.argv ) > 1:
@@ -11,37 +12,52 @@ def get_path():
 
 cmd = get_path()
 
+
+def any_of_words_in_line(words:list, line:str):
+  for word in words:
+    if word in line:
+      return True
+  return False
+
+
 class Process:
   __process = None
-  __cmd     = None
 
   stdout = subprocess.PIPE
   stderr = subprocess.PIPE
 
-  def __init__(self, cmd : str):
+  def __init__( self, cmd : str ):
     self.__process = subprocess.Popen( cmd, stdout = self.stdout, stderr = self.stderr )
-    self.__cmd     = cmd
 
-  def stderr_lines(self):
+  def stderr_lines( self ):
     for line in iter( self.__process.stderr.readline, '' ):
       yield line.decode(sys.stderr.encoding).strip()
 
-  def stdout_lines(self):
+  def stdout_lines( self ):
     for line in iter( self.__process.stdout.readline, '' ):
       yield line.decode(sys.stdout.encoding).strip()
 
-  def stop(self):
+  def stop( self ):
     self.__process.kill()
+
+  def is_alive( self ):
+    return self.__process.poll() is None
 
 
 class Daemon:
-  __is_alive = False
-  __process  = None
-  __worker1  = None
-  __worker2  = None
+  __is_alive  = False
+  __process   = None
+  __worker1   = None
+  __worker2   = None
+  __cmd       = cmd
+  __bad_words = []
+
   
-  def __init__( self, cmd:str ):
-    self.__process = Process( cmd )
+  def __init__( self, cmd:str, bad_words:list ):
+    self.__process   = Process( cmd )
+    self.__cmd       = cmd
+    self.__bad_words = bad_words
+    self.spawn()
 
   def spawn( self ):
     self.__worker1 = threading.Thread( target = lambda: self.forever(self.__process.stderr_lines(), self.on_stderr_line_read) )
@@ -53,25 +69,38 @@ class Daemon:
   def kill( self ):
     self.__is_alive = False
     self.__process.stop()
+    #self.kill_then()
+
+
+  #def kill_then( self ):
+  #  try:
+  #    self.__worker1._stop()
+  #    self.__worker2._stop()
+  #    os.kill( self.__worker2.ident )
+  #  except:
+  #    print( "kill error: {}".format(sys.exc_info()[0]) )
 
   def forever( self, range, callable ):
     while self.__is_alive:
       try:
         for e in range:
-          if not self.__is_alive:
+          if not self.__is_alive or not self.__process.is_alive():
             break
           callable( e )
-      except:
-        print( "something bad was happen" )
+      except e:
+        print( "something bad was happen: {}".format(sys.exc_info()[0]) )
+
 
   def on_stderr_line_read( self, line ):
     print( "stderr: {}".format(line) )
-    if '7' in line:
+    if any_of_words_in_line( self.__bad_words, line ):
       print( "KILLING..." )
       self.kill()
+      Daemon( self.__cmd, self.__bad_words )
 
   def on_stdout_line_read( self, line ):
     print( "stdout: {}".format(line) )
 
-daemon = Daemon( cmd )
-daemon.spawn()
+
+bad_words = ["7", "12", "13"]
+Daemon( cmd, bad_words )
